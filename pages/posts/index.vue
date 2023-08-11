f2fcc3a3c288c0fef521807f7c98595d35a5731e
Pablo Delgado <poldelgado@gmail.com>
2023-08-10 22:38:56 -0300

    add some cards with styles to show the posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -6,8 +6,33 @@ const { data: posts } = await useFetch('https://jsonplaceholder.typicode.com/pos
 <template>
     <h1 class="">POSTS PAGE</h1>
     <section>
-        <ul>
-            <li v-for="{title, id} in posts" :key="id">{{ title }}</li>
-        </ul>
+        <div class="row">
+            <article class="col-2 mb-3 d-flex align-items-stretch" v-for="{ title, id } in posts" :key="id">                
+                <div class="card cards shadow" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ title }}</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                    </div>
+                  </div> 
+            </article>        
+        </div>      
+        
     </section>
 </template>
+
+<style scoped>
+.cards{
+
+    transition: all 0.9s ease;
+    cursor: pointer;
+    
+
+  }
+.cards:hover{
+
+    box-shadow: 5px 6px 6px 2px #e9ecef;
+    transform: scale(1.05);
+}
+</style>
