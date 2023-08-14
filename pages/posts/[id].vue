ae4a9672cca59fc6d4f5fdd95d9af88c8d43c6a3
Pablo Delgado <poldelgado@gmail.com>
2023-08-14 19:50:48 -0300

    agrego componente para ver en detalle un post

---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute();
+const { data: post } = await useFetch(
+  `https://jsonplaceholder.typicode.com/posts/${route.params.id}`
+);
+</script>
+
+<template>
+  <div>
+    <h3 class="text-bold">Title:{{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
